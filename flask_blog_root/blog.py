b6751bf7583d081031e7c82d455eb844ef196276
teddymcw <teddymcw@gmail.com>
2013-12-31 18:43:43 -0800

    first iteration done, clean up some libraries

--- flask_blog_root/blog.py
+++ flask_blog_root/blog.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, session, flash, redirect, url_for, g
 import sqlite3
+from functools import wraps
 
 #temp config
 DATABASE = "fblog.db"
@@ -12,11 +13,22 @@ app = Flask(__name__)
 # pulls in config by looking for all caps
 app.config.from_object(__name__)
 
+
 # function used to connect to db, why not use the with statement?
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE'])
 
+def login_required(test):
+	@wraps(test)
+	def wrap(*args, **kwargs):
+		if 'logged_in' in session:
+			return test(*args, **kwargs)
+		else:
+			flash('You need to login first.')
+			return redirect(url_for('login'))
+	return wrap 
 
+###Views
 @app.route("/", methods=['GET','POST'])
 def login():
 	error = None
@@ -31,14 +43,43 @@ def login():
 
 
 @app.route("/main")
+@login_required
 def main():
-	return render_template('main.html')
+	g.db = connect_db()
+	cur = g.db.execute('select * from key_info_posts')
+	posts = [dict(title=row[0], post=row[1], view=row[2]) for row in cur.fetchall()]
+	g.db.close()
+	return render_template('main.html', posts=posts)
+
+@app.route("/add", methods=['POST'])
+@login_required
+def add():
+	title = request.form['title']
+	post = request.form['post']
+	view = request.form['view']
+	if not title or not post or not view:
+		flash("All fields are required. Please try again.")
+		return redirect(url_for('main'))
+	else:
+		g.db = connect_db()
+		g.db.execute('insert into key_info_posts (title, posts, views) values (?,?,?)',
+			[request.form['title'], request.form['post'], request.form['view']])
+		g.db.commit()
+		g.db.close()
+		flash('New entry was successfully posted')
+		return redirect(url_for('main'))
+
 
 @app.route('/logout')
 def logout():
 	session.pop('logged_in', None)
 	flash('You were logged out')
 	return redirect(url_for('login'))
+
+#look at foundation 5 demo sheet		
+@app.route("/test")
+def testfoun():
+	return render_template('foun5index.html')
 
 if __name__ == "__main__":
 	app.run(debug=True)
