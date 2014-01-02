d984867e28b25e03e253c18a2290ce3355275e33
teddymcw <teddymcw@gmail.com>
2014-01-02 14:33:28 -0800

    fixed bugs for heroku

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ import os
 
 from flask_blog_root.blog import app
 
-port = int(os.environ.get('POST', 5000))
+port = int(os.environ.get('PORT', 5000))
 app.run(host='0.0.0.0', port=port, debug=False)
